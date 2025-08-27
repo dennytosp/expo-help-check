bba858f29e87f936d7ea0414fdd7bf61d07ff6ea
Mad Dinh <phong.dinh2108@gmail.com>
2025-08-27 11:38:00 +0000

    :zap: Add expo-clipboard dependency

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,10 +1,23 @@
+import AntsomiRnSDK from "@antsomicorp/antsomirnsdk";
 import { useRoute } from "@react-navigation/native";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import * as Clipboard from "expo-clipboard";
+import React, { useEffect } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
 
 const Order = () => {
   const route = useRoute();
   const { id } = route.params as { id: string };
+
+  useEffect(() => {
+    AntsomiRnSDK.getPushUid().then((uid) => {
+      Alert.alert("Push UID", `${uid}`, [
+        {
+          text: "Copy",
+          onPress: () => Clipboard.setStringAsync(String(uid)),
+        },
+      ]);
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
